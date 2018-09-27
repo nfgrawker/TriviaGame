cf6bf9bcb69817e6a6f8f9848b8a12e9d34967ab
nfgrawker <nfgrawker@gmail.com>
2018-09-27 08:59:48 -0500

    building if statement on answer click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,41 @@
 $(document).ready(function(){
 var trivia1 = {
   question : "How old is Mr Burns?",
-  answer1 : "81",
-  answer2 : "104",
-  answer3 : "76",
-  answer4 : "88",
-  correct : 2
+  answers : ["81","104","76","88"],
+  correct : 1
 }
 var trivia2 = {
   question : "How old is Bart?",
-  answer1 : "11",
-  answer2 : "9",
-  answer3 : "14",
-  answer4 : "10",
-  correct : 4
+  answers : ["11","9","14","10"],
+  correct : 3
 }
 var trivia3 = {
   question : "How old is Lisa?",
-  answer1 : "8",
-  answer2 : "9",
-  answer3 : "10",
-  answer4 : "11",
-  correct : 1
+  answers : ["8","9","10","11"],
+  correct : 0
 }
 var trivia4 = {
   question : "In the Episode 'Das Bus' who does it say rescued the children?",
-  answer1 : "Otto",
-  answer2 : "Barney",
-  answer3 : "Homer",
-  answer4 : "Moe",
-  correct : 4
+  answers : ["Otto","Barney","Homer","Moe"],
+  correct : 3
 }
 var trivia5 = {
   question : "Where did Homer put his gun after Marge caught him with it?",
-  answer1 : "Bart's Treehouse",
-  answer2 : "In the Fridge",
-  answer3 : "At Moe's Tavern",
-  answer4 : "Under the couch cushion",
-  correct : 2
+  answers : ["Bart's Treehouse","In the Fridge","At Moe's Tavern","Under the couch cushion"],
+  correct : 1
 
 }
 var trivia6 = {
   question : "What is Barts twin called?",
-  answer1 : "Hugo",
-  answer2 : "Al",
-  answer3 : "Gomer",
-  answer4 : "John Jacob Jingleheimer Schmidt",
-  correct : 1
+  answers : ["Hugo","Al","Gomer","John Jacob Jingleheimer Schmidt"],
+  correct : 0
 }
 var time = 300;
 var interval = "";
 var questionArray = [trivia1, trivia2, trivia3, trivia4, trivia5, trivia6];
 var currentQuestion = 0;
 var answerArray = ["answer1","answer2","answer3","answer4"];
+var answerLetters = ["A","B","C","D"]
 
 $(".start").on("click",function() {
   newQuestion()
@@ -60,6 +43,7 @@ $(".start").on("click",function() {
 
 function newQuestion(){
   time = 300;
+  timesmaller = 10;
   interval = setInterval(count, 100);
   var newTrivia = $("<div>");
 
@@ -76,22 +60,35 @@ for (i in answerArray){
     console.log(questionArray[currentQuestion])
     answer.addClass("row mt-1 temp")
     button.addClass("col-md-3 button temp")
-    button.append('<button class="btn btn-info" type="button" name="button">Answer A</button>')
+    button.append('<button class="btn btn-info" value ="'+i+'" type="button" name="button">Answer '+answerLetters[i]+'</button>')
     answerText.addClass("col-md-9 temp")
     answerText.val("answer"+i)
-    answerText.append("<h4>- - - - - - - - -"+questionArray[currentQuestion][currentanswer]+"</h4>")
+    answerText.append("<h4>- - - - - - - - -"+questionArray[currentQuestion].answers[i]+"</h4>")
     answer.append(button)
     answer.append(answerText)
     $(".main").append(answer)
   }
+  $("btn").on("click",function(){
+    var value = $(this).val()
+    if (value == questionArray[currentQuestion].correct){
 
-
+    }
+    else 
+  })
 
   }
 
 
 function count(){
   time -=1
+  timesmaller -= 1
+  firstnumber = Math.floor(time/10)
+  secondnumber = timesmaller
+  if (timesmaller < 1){
+    timesmaller = 10
+  }
+
+  $(".timer").text(firstnumber+"."+secondnumber)
 }
 
 
