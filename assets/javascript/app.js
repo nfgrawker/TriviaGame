8d4fc772473603020582ee884ce02ed4cc1271e8
nfgrawker <nfgrawker@gmail.com>
2018-09-27 13:44:02 -0500

    Working product

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,27 +26,40 @@ var trivia5 = {
 
 }
 var trivia6 = {
-  question : "What is Barts twin called?",
+  question : "What is Bart's twin's name?",
   answers : ["Hugo","Al","Gomer","John Jacob Jingleheimer Schmidt"],
   correct : 0
 }
-var time = 300;
+var time = 1500;
 var interval = "";
 var questionArray = [trivia1, trivia2, trivia3, trivia4, trivia5, trivia6];
-var currentQuestion = 0;
+var currentQuestion = -1;
 var answerArray = ["answer1","answer2","answer3","answer4"];
 var answerLetters = ["A","B","C","D"]
+var buttonBeenClicked = false
+var correctGuesses = 0
+var wrongGuesses = 0
+var score = 0
+var timeOut = ""
+var final = ""
+var doh = new Audio("./assets/sounds/doh.mp3")
+var dohdoh = new Audio("./assets/sounds/dohdoh.mp3")
+var start = new Audio("./assets/sounds/damned.mp3")
+
 
 $(".start").on("click",function() {
   newQuestion()
+  start.play()
 })
 
 function newQuestion(){
-  time = 300;
+  buttonBeenClicked = false
+  currentQuestion += 1
+  time = 150;
   timesmaller = 10;
   interval = setInterval(count, 100);
   var newTrivia = $("<div>");
-
+  $(".temp").remove()
   $(".minheader").empty()
   newTrivia.append("<h2>"+questionArray[currentQuestion].question+"</h2>")
   newTrivia.addClass("col-md-12 question text-center temp")
@@ -68,12 +81,43 @@ for (i in answerArray){
     answer.append(answerText)
     $(".main").append(answer)
   }
-  $("btn").on("click",function(){
+  $(".btn").on("click",function(){
+    if (buttonBeenClicked == false){
     var value = $(this).val()
+    console.log("Jamaica")
     if (value == questionArray[currentQuestion].correct){
-
+      var winnerdiv = $("<div>")
+      var winnertext = $("<div>")
+      winnerdiv.addClass("loser row mt-5 temp")
+      winnertext.addClass("col-md-12 bg-success")
+      winnertext.append("<h1>Correct!</h1>")
+      winnerdiv.append(winnertext)
+      $(".main").append(winnerdiv)
+      clearInterval(interval)
+      correctGuesses +=1
+      score += time
+      dohdoh.play()
+      timeOut = setTimeout(newQuestion,3000)
+      final = setTimeout(finalScreen,1000)
     }
-    else 
+    else {
+      clearInterval(interval)
+      var loserdiv = $("<div>")
+      var losertext = $("<div>")
+      loserdiv.addClass("loser row mt-5 temp")
+      losertext.addClass("col-md-12 bg-warning")
+      losertext.append("<h1>Incorrect!</h1>")
+      loserdiv.append(losertext)
+      $(".main").append(loserdiv)
+      doh.play()
+      wrongGuesses += 1
+      score -= time
+      timeOut = setTimeout(newQuestion,3000)
+      final = setTimeout(finalScreen,1000)
+    }
+
+  }
+    buttonBeenClicked = true
   })
 
   }
@@ -88,11 +132,36 @@ function count(){
     timesmaller = 10
   }
 
-  $(".timer").text(firstnumber+"."+secondnumber)
+  $(".timer").text("Time left to Guess: "+firstnumber+"."+secondnumber)
+   if (time == 0){
+    clearInterval(interval)
+  var loserdiv = $("<div>")
+  var losertext = $("<div>")
+  loserdiv.addClass("loser row mt-5 temp")
+  losertext.addClass("col-md-12 bg-warning")
+  losertext.append("<h1>You have run out of time!</h1>")
+  loserdiv.append(losertext)
+  $(".main").append(loserdiv)
+  wrongGuesses += 1
+  timeOut = setTimeout(newQuestion,3000)
+  final = setTimeout(finalScreen,2500)
+  }
 }
 
 
-
+function finalScreen(){
+  if (currentQuestion == questionArray.length-1) {
+    clearInterval(interval)
+    clearTimeout(timeOut)
+  var finalDiv = $("<div>")
+  finalDiv.append("<h1>Score: "+score+"</h1>")
+  finalDiv.append("<h1>Correct Guesses: "+correctGuesses+"</h1>")
+  finalDiv.append("<h1>Wrong Guesses: "+wrongGuesses+"</h1>")
+  $('.main').append(finalDiv)
+  $(".temp").remove()
+  $(".timer").text("Time left to Guess: 0")
+}
+}
 
 
 
